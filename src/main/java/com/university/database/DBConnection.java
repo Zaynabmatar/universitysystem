395db9e5d921916b8ec08a3625da3dcf2b07c8db
zaynab matar <zaynabmatarchatgpt@gmail.com>
2026-08-02 15:08:54 +0300

    connect JavaFX to SQL server

---
+++ src/main/java/com/university/database/DBConnection.java
@@ -0,0 +1,48 @@
+package com.university.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Single point of access to the UniversityManagementDB database.
+ */
+public class DBConnection {
+
+    /** The one and only place the connection string is defined. */
+    private static final String CONNECTION_URL =
+            "jdbc:sqlserver://localhost\\SQLEXPRESS"
+            + ";databaseName=universitymanagementDB"
+            + ";user=sa"
+            + ";password=Zaynabmatar567"
+            + ";encrypt=true"
+            + ";trustServerCertificate=true";
+
+    private DBConnection() {
+    }
+
+    /**
+     * Opens a new connection to the database.
+     *
+     * @return an open connection the caller is responsible for closing
+     * @throws SQLException if the connection cannot be established
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(CONNECTION_URL);
+    }
+
+    /**
+     * Opens a connection and reports the result on the console.
+     */
+    public static void testConnection() {
+        try (Connection connection = getConnection()) {
+            System.out.println("Database connected successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        testConnection();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.university {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
     opens com.university to javafx.fxml;
     exports com.university;
+    exports com.university.database;
 }
